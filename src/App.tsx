4b1b030d8ab4777b4e4cb5743e33fac205bdb038
Kirk Shillingford <tkshillinz@icloud.com>
2023-12-14 15:42:04 -0700

    game data conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import styles from "./App.module.scss";
-import game_data from "./country_capitals.json";
-import { GameDataPairs } from "./types";
+// import styles from "App.module.scss";
+import all_game_data from "./country_capitals.json";
+import { GameDataPair } from "./types";
 import { useState } from "react";
 import StatBar from "./components/StatBar";
+import QuestionPrompt from "./components/QuestionPrompt";
+
+type GameDataArray = typeof all_game_data;
+type GameDataInfo = GameDataArray["game_data"][number];
+
+const toGameDataPair = (entry: GameDataInfo): GameDataPair => ({
+  Country: entry.Country,
+  Capital: Array.isArray(entry.Capital) ? entry.Capital : [entry.Capital],
+  Continent: entry.Continent,
+  Flag: entry.Flag,
+});
 
 function App() {
-  const allGameData = game_data as GameDataPairs;
-
   const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [incorrectAnswers, setIncorrectAnswers] = useState(0);
+
+  const game_data = toGameDataPair(all_game_data[currentQuestionIdx]);
 
   return (
     <div>
       <StatBar
         currentQuestion={currentQuestionIdx + 1}
-        totalQuestions={allGameData.game_data.length}
+        totalQuestions={all_game_data.game_data.length}
         correct={correctAnswers}
         incorrect={incorrectAnswers}
       />
+      <QuestionPrompt game_data={game_data} />
+      <button>Submit</button>
     </div>
   );
 }
