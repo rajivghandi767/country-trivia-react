83285c26a2e986a2f6bb47da102a7d39eb57fc9c
Rajiv Wallace <rajivghandi972@gmail.com>
2024-01-25 18:03:57 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,48 @@ import { useState } from "react";
 import StatBar from "./components/StatBar";
 import QuestionPrompt from "./components/QuestionPrompt";
 import Answer from "./components/Answer";
+import App_module from './App.module.scss';
 
 type TheJson = typeof all_game_data;
 type DataField = TheJson["data"];
 type ArrayValues = DataField[number];
 
 const toGameDataPair = (entry: ArrayValues): GameDataPair => ({
-  ...entry,
-  capital: Array.isArray(entry.capital) ? entry.capital : [entry.capital],
+    ...entry,
+    capital: Array.isArray(entry.capital) ? entry.capital : [entry.capital],
 });
 
 function App() {
-  const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
-  const [correctAnswers, setCorrectAnswers] = useState(0);
-  const [incorrectAnswers, setIncorrectAnswers] = useState(0);
+    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [incorrectAnswers, setIncorrectAnswers] = useState(0);
 
-  const data = toGameDataPair(all_game_data.data[currentQuestionIdx]);
+    const data = toGameDataPair(all_game_data.data[currentQuestionIdx]);
 
-  return (
-    <div>
-      <div>
-        <h1>Welcome to Country Trivia!</h1>
-      </div>
+    return (
+        <div className={App_module['main-container']}>
+            <div>
+                <h1>Welcome to Country Trivia!</h1>
+            </div>
 
-      <div>
-        <StatBar
-          currentQuestion={currentQuestionIdx + 1}
-          totalQuestions={all_game_data.data.length}
-          correct={correctAnswers}
-          incorrect={incorrectAnswers}
-        />
-      </div>
+            <div>
+                <StatBar
+                    currentQuestion={currentQuestionIdx + 1}
+                    totalQuestions={all_game_data.data.length}
+                    correct={correctAnswers}
+                    incorrect={incorrectAnswers}
+                />
+            </div>
 
-      <div>
-        <QuestionPrompt data={data} />
-      </div>
+            <div>
+                <QuestionPrompt data={data} />
+            </div>
 
-      <div>
-        <Answer />
-      </div>
-    </div>
-  );
+            <div>
+                <Answer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
