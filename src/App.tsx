60ab75090224bb6311e3d8ed8f3ed0e5cd54ac6d
Rajiv Wallace <rajivghandi972@gmail.com>
2024-05-08 09:40:41 -0400

    added dark mode / tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-// import styles from "App.module.scss";
+import { useEffect, useState } from "react";
 import all_game_data from "./data.json";
 import { GameDataPair } from "./types";
-import { useState } from "react";
 import StatBar from "./components/StatBar";
 import QuestionPrompt from "./components/QuestionPrompt";
 import Answer from "./components/Answer";
@@ -16,6 +15,24 @@ const toGameDataPair = (entry: ArrayValues): GameDataPair => ({
 });
 
 function App() {
+  //Dark Mode Toggle
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode === true) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  //Game Scoring Logic
+
   const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [incorrectAnswers, setIncorrectAnswers] = useState(0);
@@ -23,28 +40,37 @@ function App() {
   const data = toGameDataPair(all_game_data.data[currentQuestionIdx]);
 
   return (
-    <div>
-      <div>
-        <h1>Welcome to Country Trivia!</h1>
-      </div>
+    <>
+      <div className="h-screen bg-white dark:bg-black dark:text-white">
+        <button
+          className="absolute w-10 h-10 right-2 top-2 bg-black dark:bg-slate-300 rounded-full text-white dark:text-black"
+          onClick={toggleDarkMode}
+        >
+          {darkMode === true ? "☀️" : "🌙"}
+        </button>
 
-      <div>
-        <StatBar
-          currentQuestion={currentQuestionIdx + 1}
-          totalQuestions={all_game_data.data.length}
-          correct={correctAnswers}
-          incorrect={incorrectAnswers}
-        />
-      </div>
+        <div>
+          <h1 className="text-xl font-bold">Welcome to Country Trivia!</h1>
+        </div>
 
-      <div>
-        <QuestionPrompt data={data} />
-      </div>
+        <div>
+          <StatBar
+            currentQuestion={currentQuestionIdx + 1}
+            totalQuestions={all_game_data.data.length}
+            correct={correctAnswers}
+            incorrect={incorrectAnswers}
+          />
+        </div>
 
-      <div>
-        <Answer />
+        <div>
+          <QuestionPrompt data={data} />
+        </div>
+
+        <div>
+          <Answer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
