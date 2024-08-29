f9306943b2e04289fe62626b4b63a18eaefe27e4
Rajiv Wallace <rajivghandi972@gmail.com>
2024-08-29 16:14:04 -0400

    add submit, add answer box

--- src/components/PromptAnswer.tsx
+++ src/components/PromptAnswer.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 function PromptAnswer() {
   return (
-    <div>
-      <h1 className="text-center p-5">
+    <div className="text-center p-5">
+      <h1>
         What is the {} of {}
       </h1>
+      <input
+        className="border-2 border-black rounded m-1 p-1"
+        type="text"
+        placeholder="Type your answer here..."
+      />
+      <div>
+        <button className="border-2 border-black rounded p-1">Submit</button>
+      </div>
     </div>
   );
 }
