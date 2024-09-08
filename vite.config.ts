55fcfe332bd97cfa29cbc9ae937b49161b7c3b7a
Rajiv Wallace <rajivghandi972@gmail.com>
2024-09-08 17:20:29 -0400

    update config files

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    server: {
+    host: true,
+  },
+  preview: {
+    host: true,
+    port: 5000,
+  },
 });
